a7553a64b5d53684345bd66280650f11f600979d
ismail-png-1 <70921008+ismail-png-1@users.noreply.github.com>
2023-05-28 17:38:22 +0100

    Update autosetngrok.sh

--- autosetngrok.sh
+++ autosetngrok.sh
@@ -1,6 +1,6 @@
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
-echo "Auto Setup Ngrok Token"
+echo "2QQglVj6MmvJ5gIAXAztG1qF2Cx_cAShaUKzbZNZTYv7CtAP"
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
